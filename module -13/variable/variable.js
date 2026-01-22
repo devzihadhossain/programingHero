9cd88a50f905122c5c3df168e54be4b87dd88d29
zihadcu <zihadcu@gmail.com>
2026-01-23 02:46:27 +0600

    Learning Javascript

---
+++ module -13/variable/variable.js
@@ -0,0 +1,23 @@
+// var age = 25;
+// var price = 300;
+// var year = 2025;
+// var money = 10000;
+// console.log(money);
+// var none;
+
+// var money = 1120;
+
+// console.log(money);
+// console.log(none);
+
+// console.log(age);
+// console.log(year);
+
+// var phone = 01554410401;
+
+// console.log(phone);
+
+var age = 20;
+
+age = 25;
+console.log(age);
