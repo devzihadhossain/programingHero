c531c5d0025ee43e549243864bc81d0e2f832f97
zihadcu <zihadcu@gmail.com>
2026-01-23 16:53:16 +0600

    Learning Js

---
+++ module -13/naming/naming.js
@@ -0,0 +1,39 @@
+// Cannot use reserved keyword as a variable name     Ex. var var = 10
+// Cannot use gap or space in variable name     Ex. my variable
+// Cannot use Quotation in variable name     Ex. "1variable"
+// Cannot use number on the start of a variable     Ex. 1variable
+
+// JavaScript is Case Sensitive
+
+// How to write variable name using Multiple Word
+/*
+    1. Use underscore in variable name     Ex. my_variable  *snake case*
+    1. Use the 2nd to last word 1st alphabet capitale letter in variable name     Ex. myVariable  *camel case*
+    1. Use all word 1st alphabet Capitale letter in variable name     Ex. MyVariable  *Pascal case*
+*/
+
+/* 
+
+    there are some words you cannot use as a variable name in JavaScript. those are called Reserved Keywords.
+
+*/
+
+/*
+
+    JavaScript Keywords
+
+    1) Keywords are a set of Reserved Keywords.
+    2) Cannot be used as names of Functions, labels, or variables.
+
+    All Characters of the keyword is small letter.
+
+    Variable naming conventions
+
+    1) camelCase
+    2) snake_case
+    3) PascalCase
+
+
+    Cannot use Symbol in the start of a variable name
+
+*/
